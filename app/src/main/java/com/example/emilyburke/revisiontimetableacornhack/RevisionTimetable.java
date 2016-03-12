cbd97cc49f2c59e070eaf4cc4d5d05661d0442d9
evb5226 <evb5226@psu.edu>
2016-03-12 08:16:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/emilyburke/revisiontimetableacornhack/RevisionTimetable.java
@@ -0,0 +1,13 @@
+package com.example.emilyburke.revisiontimetableacornhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RevisionTimetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revision_timetable);
+    }
+}
